ed7aeb8932a2ccae05e1c1164c94a8677185c829
Tommy Christenson <tchristenson21@gmail.com>
2023-10-08 15:46:26 -0500

    Removed unnecessary functions and simplified code

--- cargo_challenge.py
+++ cargo_challenge.py
@@ -2,55 +2,6 @@ WIDTH = 10
 HEIGHT = 53
 EMPTY = '_'
 
-# Cargo Shapes
-O = [
-    ['O', 'O'],
-    ['O', 'O'],
-]
-
-I = [
-    ['I'],
-    ['I'],
-    ['I'],
-    ['I'],
-]
-
-S = [
-    ['_', 'S', 'S'],
-    ['S', 'S', '_'],
-]
-
-Z = [
-    ['Z', 'Z', '_'],
-    ['_', 'Z', 'Z'],
-]
-
-L = [
-    ['L', '_'],
-    ['L', '_'],
-    ['L', 'L'],
-]
-
-J = [
-    ['_', 'J'],
-    ['_', 'J'],
-    ['J', 'J'],
-]
-
-T = [
-    ['T', 'T', 'T'],
-    ['_', 'T', '_'],
-    ['_', 'T', '_'],
-]
-
-cargo_shapes = {'O': O,
-                'I': I,
-                'S': S,
-                'Z': Z,
-                'L': L,
-                'J': J,
-                'T': T}
-
 deltas = {'O': [(0, 0), (0, -1), (1, 0), (1, -1)],
                 'I': [(0, 0), (0, -1), (0, -2), (0, -3)],
                 'S': [(0, 0), (1, 0), (1, -1), (2, -1)],
@@ -61,8 +12,8 @@ deltas = {'O': [(0, 0), (0, -1), (1, 0), (1, -1)],
 
 trailer = [[EMPTY] * WIDTH for _ in range(HEIGHT)]
 
-def get_valid_start(trailer, letter, x, y):
 
+def get_valid_start(trailer, letter, x, y):
     while y >= 0:
         all_positions_valid = True
 
@@ -92,71 +43,27 @@ def get_valid_start(trailer, letter, x, y):
     return None
 
 
-
-def get_cargo_positions(string_shape, x):
-    shape = cargo_shapes[string_shape]
-    positions = {}
-    shape_positions = []
-
-    for row in range(len(shape)):
-        for column in range(len(shape[0])):
-            if shape[row][column] == string_shape:
-                shape_positions.append((row, column))
-    positions[string_shape] = shape_positions
-
-    print('positions ---->', positions)
-    fill_trailer(trailer, positions, x)
-
-def add_cargo(letter, x, y):
-    if letter == 'O':
-        trailer[y][x] = letter
-        trailer[y - 1][x] = letter
-        trailer[y][x + 1] = letter
-        trailer[y - 1][x + 1] = letter
-    elif letter == 'I':
-        trailer[y][x] = letter
-        trailer[y - 1][x] = letter
-        trailer[y - 2][x] = letter
-        trailer[y - 3][x] = letter
-    elif letter == 'S':
-        trailer[y][x] = letter
-        trailer[y][x + 1] = letter
-        trailer[y - 1][x + 1] = letter
-        trailer[y - 1][x + 2] = letter
-    elif letter == 'Z':
-        trailer[y][x] = letter
-        trailer[y][x + 1] = letter
-        trailer[y + 1][x + 1] = letter
-        trailer[y + 1][x + 2] = letter
-    elif letter == 'L':
-        trailer[y][x] = letter
-        trailer[y - 1][x] = letter
-        trailer[y - 2][x] = letter
-        trailer[y - 2][x + 1] = letter
-    elif letter == 'L':
-        trailer[y][x] = letter
-        trailer[y][x + 1] = letter
-        trailer[y + 1][x + 1] = letter
-        trailer[y + 2][x + 1] = letter
-    elif letter == 'T':
-        trailer[y][x] = letter
-        trailer[y][x + 1] = letter
-        trailer[y][x + 2] = letter
-        trailer[y - 1][x + 1] = letter
-
-def fill_trailer(trailer, positions, x):
+def fill_trailer(trailer, letter, x):
     print(trailer)
-    letter = list(positions.keys())[0]
+    # letter = list(positions.keys())[0]
     start_pos = get_valid_start(trailer, letter, x, HEIGHT - 1)
     x, y = start_pos
     print('x:', x)
     print('y:', y)
     print('letter ---->', letter)
-    add_cargo(letter, x, y)
+    for delta in deltas[letter]:
+        delta_row, delta_column = delta
+        print('delta_row ---->', delta_row)
+        print('delta_column ---->', delta_column)
+        neighbor_row = x + delta_row
+        neighbor_column = y + delta_column
+        trailer[neighbor_column][neighbor_row] = letter
+
 
 def print_trailer():
     for row in trailer:
         print(' '.join(row))
+
 
 def main(entries):
     entries = entries.split(',')
@@ -164,10 +71,12 @@ def main(entries):
     for entry in entries:
         # print('entry ---->', entry)
         x = int(entry[0])
-        shape = entry[1]
+        letter = entry[1]
         # print('x-axis:', x, 'shape:', shape)
-        get_cargo_positions(shape, x)
+        fill_trailer(trailer, letter, x)
+        # get_cargo_positions(letter, x)
     print_trailer()
+
 
 main('0O,2I,3S')
 # main('7S,7I,5Z')
